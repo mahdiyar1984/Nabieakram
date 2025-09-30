30e53dbae1a970ed4de90f01ec72234c0be5fa1d
Mahdi Mohammadpour <103903721+mahdiyar1984@users.noreply.github.com>
2025-09-30 16:48:09 +0330

    complete article management in admin django

--- static/js/custom.js
+++ static/js/custom.js
@@ -24,3 +24,20 @@ document.addEventListener('DOMContentLoaded', function() {
         });
     }
 });
+
+function toggleDropdown() {
+      var menu = document.getElementById('dropdownMenu');
+      if (menu.style.display === 'block') {
+        menu.style.display = 'none';
+      } else {
+        menu.style.display = 'block';
+      }
+    }
+    // Optional: Close dropdown if clicked outside
+    document.addEventListener('click', function(event) {
+      var toggle = document.getElementById('dropdownToggle');
+      var menu = document.getElementById('dropdownMenu');
+      if (!toggle.contains(event.target) && !menu.contains(event.target)) {
+        menu.style.display = 'none';
+      }
+    });
